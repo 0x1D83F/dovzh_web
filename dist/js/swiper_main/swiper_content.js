916c2e98a4beaf36a70e5c80fcaec95aef868360
0x d83f <alex.borrovikov@gmail.com>
2021-02-20 19:23:46 +0200

    delete empty files(_clone,change, pass)

--- dist/js/swiper_main/swiper_content.js
+++
@@ -1,5 +0,0 @@
-function swiperContentHeight(){
-    let logo = document.querySelector('.slide_main__type img').offsetHeight;
-    let elem = document.querySelectorAll('.slide_main__content-text');
-    elem.forEach(element => element.style.height = `${logo}px`)
-}
